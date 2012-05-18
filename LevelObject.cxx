080aa430e45de53a7a26404b6f7b91815617210a
Paul Neel <neelp@students.wwu.edu>
2012-05-18 14:27:48 -0700

    Updated LevelObject.h and started LevelObject.cxx

---
+++ LevelObject.cxx
@@ -0,0 +1,24 @@
+// Students: Nick Tarter & Paul Neel
+// Class: CSCI 245
+// Assignment: 4
+
+#include "LevelObject.h"
+
+LevelObject::IsVisible()
+{
+   return visible;
+}
+
+LevelObject::SetVisible()
+{
+   visible = true;
+}
+
+Trap::Activate(Player p)
+{
+   if (type = ARROW)
+   {
+   //
+   }
+      
+}
