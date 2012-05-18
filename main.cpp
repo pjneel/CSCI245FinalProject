63bbe728dc69d12f47711f10ba854a412fa54d1f
Nick Tarter <ar2server@gmail.com>
2012-05-18 14:51:01 -0700

    Uploading main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include "LevelObject.h"
+#include "Level.h"
+#include "Room.h"
+#include "Player.h"
+#include "Monster.h"
+
+using namespace std;
+
+int main()
+{
+	cout << testInt;
+	return 0;
+}
